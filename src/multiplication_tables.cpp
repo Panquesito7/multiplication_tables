cf6ca6d41ba23dc9f9a2f9af910079d8a57ea22a
David Leal <halfpacho@gmail.com>
2023-12-24 11:41:45 -0600

    Update multiplication_tables.cpp

--- src/multiplication_tables.cpp
+++ src/multiplication_tables.cpp
@@ -1,9 +1,19 @@
 /**
- * @file
- * @brief [Multiplication Table](https://en.wikipedia.org/wiki/Multiplication_table) for any number.
- * Provides a self-test mode using `assert` and an interactive mode which lets you choose any number.
+ * Shows the multiplication tables for any number with interactive and self-tests mode.
+ * Copyright (C) 2020-2023 David Leal (halfpacho@gmail.com)
  *
- * @author [David Leal](https://github.com/Panquesito7)
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include <iostream>   /// for IO operations
